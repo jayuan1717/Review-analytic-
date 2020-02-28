9624a12cc21a9e5304c4292527b04633bda3f7e1
Jay Yuan <hayua7435@gmail.com>
2020-02-28 04:39:17 -0500

    add filter function

--- read.py
+++ read.py
@@ -12,4 +12,10 @@ for d in data:
 	sum_len = sum_len + len(d)
 	avg = sum_len/len(data)
 print(sum_len)
-print(avg)
+print('留言的平均長度為', avg)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '比留言長度小於100')
